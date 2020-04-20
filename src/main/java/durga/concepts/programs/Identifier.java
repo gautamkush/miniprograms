d75d99511a83477966e9db25fc574230327204d8
kush.gautam <gautamkush1854@gmail.com>
2020-04-20 20:49:37 +0530

    identifiers

--- src/main/java/durga/concepts/programs/Identifier.java
+++ src/main/java/durga/concepts/programs/Identifier.java
@@ -4,6 +4,9 @@ public class Identifier {
 	//Name in java is identifier - class, method , function
 	int a=10;
 	int A=12;// java is casesensitive prog language
+	//int if=10; - CE We cant use reserved words as qualifiers
+	int String=10;
+	int Runnable = 20;
 	// NO LENGTH LIMIT FOR IDENTIFIER
 	// a is an identifier
 	// a-zA-Z0-9$_
